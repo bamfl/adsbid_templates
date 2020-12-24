9c785b10179575cc7193a6a05315aefbc2140d5a
dmitry.petuhov <dmitry.petuhov@profitclicks.ru>
2020-12-24 12:54:04 +0300

    Убрал промежуточное состояние, изменил размер картинок

--- app/js/main.js
+++ app/js/main.js
@@ -6,36 +6,34 @@ $(function () {
 			dots: false,
 			arrows: true,
 			accessibility:false,
-			slidesToShow: 4.1,
+			slidesToShow: 5.2,
 			slidesToScroll: 2,
 			cssEase: 'ease-out',
 			speed: 700,
-			//autoplay: true,
-			// autoplaySpeed: 3000,
-			// adaptiveHeight: true,
-			//asNavFor:'',
-			//appendDots:
-			//appendArrows:$('.mainslider-arrows .container'),
-			// nextArrow:'<button type="button" class="slick-next"></button>',
-			// prevArrow:'<button type="button" class="slick-prev"></button>',
 			responsive: [
 				{
 					breakpoint: 1024,
 					settings: {
-						slidesToShow: 3.1
+						slidesToShow: 4.2
+					}
+				},
+				{
+					breakpoint: 800,
+					settings: {
+						slidesToShow: 3.2
 					}
 				},
 				{
 					breakpoint: 600,
 					settings: {
-						slidesToShow: 2.1
+						slidesToShow: 2.2
 					}
 				},
 				{
 					breakpoint: 426,
 					settings: {
-						slidesToShow: 1,
-						slidesToScroll: 1.5
+						slidesToShow: 1.5,
+						slidesToScroll: 1
 					}
 				}
 			]
